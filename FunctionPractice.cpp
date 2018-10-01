9b6a3a7e676780add8d6177f7a6933c1fdc65c7e
ayueha <ayueha@ttu.ee>
2018-10-01 10:35:06 +0300

    Initial Commit

---
+++ FunctionPractice.cpp
@@ -0,0 +1,214 @@
+#include <iostream>  // input and output
+#include <typeinfo>  //Dynamic cast
+#include <string>    //sequences of charactors
+#include <cstdlib>   // include exchange lib of string to integer
+#include <Windows.h> // Sleep function
+#include <cctype>    //classify and transform individual chars
+#include <algorithm> //define collection of functiond ,specia;ized for  range of eleent
+
+using namespace std;
+
+void chooseOption();
+void addition();
+void substraction();
+bool velification(string x);
+string inputLoop();
+void multipication();
+void division();
+void power();
+void comparFloat();
+
+int main() {
+	chooseOption();
+	Sleep(4 * 1000);
+	return 0;
+}
+
+
+void chooseOption() {
+	string proc;
+	bool valid = false;
+	string flag="N";
+	
+
+	cout << "This is simple calsulator ; plase choose your option below" << endl;
+	cout << "Enter  [+] --> Adission" << endl;
+	cout << "Enter  [-] --> Substraction" << endl;
+	cout << "Enter  [*] --> Multiplication" << endl;
+	cout << "Enter  [/] --> Division" << endl;
+	cout << "Enter  [^] --> Power" << endl;
+	cout << "Enter  [C] --> Comparision of five float figures" << endl;
+	cout << "Choose your option" << endl;
+	cin >> proc;
+
+	
+	while (valid==false && flag!="Y") {
+
+		if (proc == "+") {
+			cout << "Adission has been choosed" << endl;
+			addition();
+		}
+		else if (proc == "-") {
+			cout << "Substraction has been choosed" << endl;
+			substraction();
+		}
+		else if (proc == "*") {
+			cout << "Multiplication has been choosed" << endl;
+			multipication();
+		}
+		else if (proc == "/") {
+			cout << "Division has been choosed" << endl;
+			division();
+		}
+		else if (proc == "^") {
+			cout << "Power has been choosed" << endl;
+			power();
+		}
+		else if (proc == "C") {
+			cout << "Comparison process has been choosed" << endl;
+			comparFloat();
+		}
+		else {
+			cout << "Please Enter + , - , / , * or ^" << endl;
+		}
+
+		
+		cout << "Do you want to stop simulation? chose [Y]or[y] / or type any alphabet key to continue" << endl;
+		cin >> flag;
+		transform(flag.begin(), flag.end(), flag.begin(), toupper);
+
+			if (flag == "Y" ) {
+				valid = true;
+				cout << "This program automatically closed ..." << endl;
+			}
+			else {
+				cout << "Enter  [+] --> Adission" << endl;
+				cout << "Enter  [-] --> Substraction" << endl;
+				cout << "Enter  [*] --> Multiplication" << endl;
+				cout << "Enter  [/] --> Division" << endl;
+				cout << "Enter  [^] --> Power" << endl;
+				cout << "Enter  [C] --> Comparision of five float figures" << endl;
+				cout << "Choose your option" << endl;
+				cin >> proc;
+			}
+
+	}
+
+}
+
+
+
+
+void addition() {
+	cout << "addision process start..." << endl;
+	string a, b;
+
+	a = inputLoop();
+	b = inputLoop();
+
+	int numSum;
+	numSum = std::stoi(a) + std::stoi(b);
+	cout << "result of addition is ..." << numSum << endl;
+}
+
+string inputLoop() {
+	bool result = false;
+	string x;
+	while (result == false) {
+		cout << "Enter a number" << endl;
+		cin >> x;
+		result = velification(x);
+	}
+	return x;
+}
+
+bool velification(string x) {
+	bool result = true;
+	for (int i = 0; i < x.length(); i++) {
+		if (!(x[i] > 47 && x[i] < 58)) {
+			cout << x[i] << " Invalid" << endl;
+			result = false;
+		}
+	}
+	return result;
+}
+
+void substraction() {
+	cout << "substraction process start..." << endl;
+	string a, b;
+
+	a = inputLoop();
+	b = inputLoop();
+
+	int numSum;
+	numSum = std::stoi(a) - std::stoi(b);
+	cout << "result of substraction  is ..." << numSum << endl;
+
+}
+
+
+void multipication() {
+	cout << "multipication process start..." << endl;
+	string a, b;
+
+	a = inputLoop();
+	b = inputLoop();
+
+	int numSum;
+	numSum = std::stoi(a) * std::stoi(b);
+	cout << "result of multiplication  is ..." << numSum << endl;
+}
+
+
+void division() {
+	cout << "division process start..." << endl;
+	string a, b;
+	int tmpa;
+
+	a = inputLoop();
+	b = inputLoop();
+
+	tmpa = stoi(a);
+	while (tmpa <1) {
+		cout << "0 can not divide... plase enter again" << endl;
+		a = inputLoop();
+		tmpa = stoi(a);
+	}
+
+	double numSum;
+	numSum = stod(a) / stod(b);
+	cout << "result of division  is ..." << numSum << endl;
+
+}
+
+
+void power() {
+	cout << "power process start..." << endl;
+	string a, b;
+	int numSum =1 ;
+	a = inputLoop();
+	b = inputLoop();
+
+	for (int i = 1; i <= stoi(b); i++) {
+		numSum = numSum * stoi(a);
+	}
+	//numSum = stoi(a)  stoi(b);
+	cout << "result of power  is ..." << numSum << endl;
+
+}
+
+void comparFloat() {
+	const int i = 5;
+	int j=0;
+	float arrFloar[i];
+	
+	while (j < i) {
+		cout << "Enter your No"<< j+1 <<" floating number" << endl;
+		cin >> arrFloar[j];
+		j++;
+	}
+
+	sort(arrFloar, arrFloar + i);
+	cout << "MAX number is... " << arrFloar[i-1] << endl;
+	cout << "MIN number is... " << arrFloar[0] << endl;
+}
---
+++ basicPractice.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int basicContents(void) {
+
+	int A;
+	A = 4;
+	// cout << "Hello" << endl;
+	cout << A << endl;
+	cout << &A << endl;
+	int B;
+	B = 10;
+	cout << B << endl;
+	cout << &B << endl;
+
+	int c = 20, d = 0, f = 40; //4 bytes memory
+	cout << c << " : address of c :" << &c << endl;
+	cout << d << " : address of d :" << &d << endl;
+	cout << f << " : address of f :" << &f << endl;
+
+	short t1 = 5; // 2bytes
+	cout << t1 << " : address of t1 :" << &t1 << endl;
+	
+	//float t2 = 5.12; //4Bytes
+	//double t3 = 5.12; //8Bytes
+
+	char st; //string charactor
+	st = 'A';
+	cout << st << endl;
+
+	string stringLong;
+	string stringLongLatter;
+	stringLong = "ABCDEFGH";
+	stringLongLatter = "IJKLMN";
+	cout << stringLong + " : " + stringLongLatter << endl;
+
+	//unsigned int plus only
+	unsigned short t7 = 470l;
+	cout << t7 << endl;
+
+	/*
+	string inputedLetters;
+	cout << "Enter some words" << endl;
+	cin >> inputedLetters;
+	cout << inputedLetters << endl;
+	*/
+
+	int former, latter;
+	former = 10;
+	latter = 3;
+
+	cout << latter % former << endl;
+
+	
+
+	// bitwise
+	/*
+	AND - &
+	OR  - |
+	NOT - ~
+	XOR- ^
+	bitwise left shift <<
+	bitwise right shift
+	*/
+
+	cout << (10 & 2) << endl;
+
+	/*
+	1 0 1 0
+	0 0 1 0 AND operator
+	0 0 1 0
+	*/
+
+	cout << (11 | 2) << endl;
+	/*
+	1 0 1 1
+	0 0 1 0 OR operator
+	1 0 1 1
+	*/
+
+	cout << (10 ^ 3) << endl;
+	/*
+	1 0 1 0
+	0 0 1 1 XOR operator
+	1 0 0 1
+	*/
+
+	cout << (~10) << endl;
+	/*
+	00000000000 1010
+	11111111111 0101 (minus)
+	*/
+
+	cout << (10 << 1) << endl; // 20* 2^0
+	/*
+	1 0 1 0
+  1 0 1 0 0  -> Multiplay
+	*/
+	cout << (10 << 2) << endl; //20 * 2^1
+
+	cout << (10 << 3) << endl; //20 * 2 *2
+
+	//int arrint;
+
+
+	/*
+	int intA , intB;
+	bool valid = false;
+	cout << "Enter a number " << endl;
+	cin >> intA;
+	while (valid){
+
+		if (cin.good()){
+			break;
+		}else{
+			cin.clear();
+			cout << "Enter a number " << endl;
+			cin >> intA;
+		}
+	}
+
+	cout << "Enter a number which you want to compare" << endl;
+	cin >> intB;
+	while (true){
+		cin >> intB;
+		if (cin.good()){
+			break;
+		}else{
+			cout << "Enter a number " << endl;
+		}
+	}
+
+	if (intA > intB) {
+		cout << intA << " is larger" << endl;
+	}else{
+		cout << intB << " is larger" << endl;
+	}
+
+	*/
+
+
+	//Array practice
+	int arr[3]; //0,1,2
+
+	arr[0] = 10;
+	arr[1] = 40;
+	arr[2] = 60;
+
+	cout << arr[0] << "Address is " << &arr[0] << endl; //4byte each
+	cout << arr[1] << "Address is " << &arr[1] << endl; //4byte each
+	cout << arr[2] << "Address is " << &arr[2] << endl; //4byte each
+
+
+	int subarr[3][4] = { 0 }; //All 0 set
+
+	cout << &subarr[0] << endl;
+	cout << &subarr[0][0] << endl;
+	cout << &subarr[0][1] << endl;
+	cout << &subarr[0][2] << endl;
+	cout << &subarr[0][3] << endl;
+	cout << "-----" << endl;
+	cout << &subarr[1] << endl;
+	cout << &subarr[1][0] << endl;
+
+	for (int i = 0; i < 5; i++) {
+		cout << &i << "value of i is " << i << endl;
+	}
+
+	return 0;
+}
